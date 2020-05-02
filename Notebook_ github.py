9a6dbdc61f5277549225d11d8195d15f73ca84dc
sreelatha22 <sreelatha2204@gmail.com>
2020-05-02 20:32:39 +0530

    Add files via upload

---
+++ Notebook_ github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This notebook will be mainly used for Applied Data Science Capstone project
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
